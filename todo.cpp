e47b45b1ecf2e04f684c136ead6fc5c9eea5f2e5
Shahid Yousuf <shahidyousuf77@gmail.com>
2022-10-20 22:33:23 +0530

    initialize c++ 17 todos app

---
+++ todo.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Shahid Yousuf on 20/10/22.
+//
+
+#include "todo.h"
---
+++ todo.h
@@ -0,0 +1,8 @@
+//
+// Created by Shahid Yousuf on 20/10/22.
+//
+
+#ifndef CPP_TODOS_TODO_H
+#define CPP_TODOS_TODO_H
+
+#endif //CPP_TODOS_TODO_H
